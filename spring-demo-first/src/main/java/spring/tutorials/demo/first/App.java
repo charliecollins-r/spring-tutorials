f18a3be0cca7f9c34679c5b4d17ad86c42b24492
charliecollins-r <charliecollins.r@gmail.com>
2018-11-24 14:08:05 -0600

    Syncing versions of Spring framework dependencies in POM. Fetching Coach object by ClassPathXmlApplicationContext.

--- spring-demo-first/src/main/java/spring/tutorials/demo/first/App.java
+++ spring-demo-first/src/main/java/spring/tutorials/demo/first/App.java
@@ -1,14 +1,22 @@
 package spring.tutorials.demo.first;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Main application
  */
 public class App {
     public static void main(String[] args) {
-        // Create the object
-        Coach baseballCoach = new TrackCoach();
+        // Load the Spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // Use the object
-        System.out.println(baseballCoach.getDailyWorkout());
+        // Retrieve bean from Spring container
+        Coach coach = context.getBean("myCoach", Coach.class);
+
+        // Call methods on the bean
+        System.out.println(coach.getDailyWorkout());
+
+        // Close the context
+        context.close();
     }
 }
